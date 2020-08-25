b425fbb11d4a73c60c3882e912218a8e5bbb5685
dsavo <63007773+denosav@users.noreply.github.com>
2020-08-25 17:39:25 +0100

    Some work done

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Get, Param, Post, Body, HttpCode, HttpStatus, Res } from '@nestjs/common';
+import { response } from 'express';
+
+@Controller('coffees')
+export class CoffeesController {
+    @Get("")
+    findAll (@Res() response) {
+        return "This action returns all coffees";
+    }
+
+    @Get(":id")
+    findOne (@Param("id") id: string) {
+        return `This action returns #${id} coffee`;
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.GONE)
+    create (@Body() body){
+        return body;
+    } 
+}
