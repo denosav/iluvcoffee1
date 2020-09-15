7aec2fad22264dddace2146d1db794fedc8c79e0
dsavo <63007773+denosav@users.noreply.github.com>
2020-09-15 14:31:42 +0100

    Fixed postgres errors - continuing

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,26 @@
+/* CoffeesModule - FINAL CODE */
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
 
-@Module({controllers: [CoffeesController], providers: [CoffeesService]})
+@Module({
+  imports: [TypeOrmModule.forFeature([Coffee])], // 👈 Adding Coffee Entity here to TypeOrmModule.forFeature
+  controllers: [CoffeesController],
+  providers: [CoffeesService],
+})
 export class CoffeesModule {}
+
+/*
+Generated Coffee table in PostgreSQL Database
+
++-------------+--------------+----------------------------+
+|                          coffee                         |
++-------------+--------------+----------------------------+
+| id          | int(11)      | PRIMARY KEY AUTO_INCREMENT |
+| name   	    | varchar      |                            |
+| brand       | varchar      |                            |
+| flavors     | json         |                            |
++-------------+--------------+----------------------------+
+*/
