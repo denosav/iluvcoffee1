b0f07f5a3d8083385627ee1a0d96d191b3e5b717
dsavo <63007773+denosav@users.noreply.github.com>
2020-10-15 11:33:40 +0100

    Adding retrieval of relations for entities

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -15,12 +15,16 @@ export class CoffeesService {
     ) {}
 
     findAll () {
-        return this.coffeeRepository.find();
+        return this.coffeeRepository.find({
+            relations: ['flavors']
+        })
     }
     
     async findOne (id: string) {
 //        throw `A random error`;
-        const coffee = await this.coffeeRepository.findOne(id);
+        const coffee = await this.coffeeRepository.findOne(id, {
+            relations: ['flavors'],
+        });
         if (!coffee) {
             throw new NotFoundException(`Coffee #${id} not found`);
         }
