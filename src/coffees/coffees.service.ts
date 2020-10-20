8078370e023c5dd5550beeaeb8c928d6aa1fe0f5
dsavo <63007773+denosav@users.noreply.github.com>
2020-10-20 11:43:45 +0100

    Added use of transactions via coffee recommendations function

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -5,7 +5,7 @@ import { Flavor } from './entities/flavor.entity';
 import { CreateCoffeeDto } from './dto/create-coffee.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UpdateCoffeeDto } from './dto/update-coffee.dto';
-import { Repository } from 'typeorm';
+import { Connection, Repository } from 'typeorm';
 import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 
 @Injectable()
@@ -16,6 +16,7 @@ export class CoffeesService {
         private readonly coffeeRepository: Repository<Coffee>,
         @InjectRepository(Flavor)
         private readonly flavorRepository: Repository<Flavor>,
+        private readonly connection: Connection,
     ) {}
 
     findAll (paginationQuery: PaginationQueryDto) {
@@ -71,6 +72,30 @@ export class CoffeesService {
     async remove (id: string) {
         const coffee = await this.findOne(id);
         return this.coffeeRepository.remove(coffee);
+    }
+
+    async recommendCoffee (coffee: Coffee) {
+        const queryRunner = this.connection.createQueryRunner();
+
+        await queryRunner.connect();
+        await queryRunner.startTransaction();
+        try{
+            coffee.recommendations++;
+
+            const recommendEvent = new Event();
+            recommendEvent.name = 'recommend_coffee';
+            recommendEvent.type = 'coffee';
+            recommendEvent.payload = {coffeeId: coffee.id};
+
+            await queryRunner.manager.save(coffee);
+            await queryRunner.manager.save(recommendEvent);
+
+            await queryRunner.commitTransaction();
+        } catch (err) {
+            await queryRunner.rollbackTransaction();
+        } finally {
+            await queryRunner.release();
+        }
     }
 
     private async preloadFlavorByName(name: string): Promise<Flavor>{
