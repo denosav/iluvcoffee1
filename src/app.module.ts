aee5b491ce1fe7d7f2f6aad763fc161bc0202f55
dsavo <63007773+denosav@users.noreply.github.com>
2020-11-12 12:55:25 +0000

    Added asynchronous config of dynamic modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,18 @@ import appConfig from './config/app.config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres', // type of our database
+        host: process.env.DATABASE_HOST, // database host
+        port: +process.env.DATABASE_PORT, // database host
+        username: process.env.DATABASE_USER, // username
+        password: process.env.DATABASE_PASSWORD, // user password
+        database: process.env.DATABASE_NAME, // name of our database,
+        autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
+        synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)        
+      }),
+    }),
     ConfigModule.forRoot({
       load: [appConfig],
 //      ignoreEnvFile: true,
@@ -23,16 +35,6 @@ import appConfig from './config/app.config';
 */
     }),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres', // type of our database
-      host: process.env.DATABASE_HOST, // database host
-      port: +process.env.DATABASE_PORT, // database host
-      username: process.env.DATABASE_USER, // username
-      password: process.env.DATABASE_PASSWORD, // user password
-      database: process.env.DATABASE_NAME, // name of our database,
-      autoLoadEntities: true, // models will be loaded automatically (you don't have to explicitly specify the entities: [] array)
-      synchronize: true, // your entities will be synced with the database (ORM will map entity definitions to corresponding SQL tabled), every time you run the application (recommended: disable in the production)
-    }),
     CoffeeRatingModule,
     DatabaseModule,
   ],
