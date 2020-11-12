410b16cc1e4638d02fbfb6feed5b8f34c4a5b7c4
dsavo <63007773+denosav@users.noreply.github.com>
2020-11-12 11:19:42 +0000

    Added config schema validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,17 @@ import { CoffeesModule } from './coffees/coffees.module';
 import { CoffeeRatingModule } from './coffee-rating/coffee-rating.module';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
 //      ignoreEnvFile: true,
-      envFilePath: '.env',
+//      envFilePath: '.env',
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.required(),
+        DATABASE_PORT: Joi.number().default(5432),
+      }),
     }),
     CoffeesModule,
     TypeOrmModule.forRoot({
