a571053e77c2cd48b6d773006d96ac56abb3fecb
dsavo <63007773+denosav@users.noreply.github.com>
2020-11-19 10:26:29 +0000

    Added timeout interceptor

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -23,8 +23,9 @@ export class CoffeesController {
     @Public()
     @UsePipes(ValidationPipe)
     @Get("")
-    findAll (@Query() paginationQuery: PaginationQueryDto) {
+    async findAll (@Query() paginationQuery: PaginationQueryDto) {
         //   const { limit, offset } = paginationQuery;
+        await new Promise(resolve => setTimeout(resolve, 5000));
         return this.coffeesService.findAll(paginationQuery);
     }
 
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ApiKeyGuard } from './common/guards/api-key.guard';
 import { WrapResponseInterceptor } from './common/interceptors/wrap-response.interceptor';
+import { TimeoutInterceptor } from './common/interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -18,7 +19,7 @@ async function bootstrap() {
     }),
   );
 //  app.useGlobalGuards(new ApiKeyGuard());
-  app.useGlobalInterceptors(new WrapResponseInterceptor());
+  app.useGlobalInterceptors(new WrapResponseInterceptor(), new TimeoutInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(3000);
 }
