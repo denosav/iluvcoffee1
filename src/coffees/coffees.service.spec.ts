b3fe1300bb218ef1dfe3ac40b0df0afd37a7a5ed
dsavo <63007773+denosav@users.noreply.github.com>
2020-12-17 14:12:03 +0000

    Working unit test, but without Flavor

--- src/coffees/coffees.service.spec.ts
+++ src/coffees/coffees.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoffeesService } from './coffees.service';
+import { Coffee } from './entities/coffee.entity';
+import { Connection } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('CoffeesService', () => {
   let service: CoffeesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CoffeesService],
+      providers: [
+        CoffeesService,
+        { provide: Connection, useValue: {} },
+//        { provide: getRepositoryToken(Flavor), useValue: {} },
+        { provide: getRepositoryToken(Coffee), useValue: {} },
+      ],
     }).compile();
 
     service = module.get<CoffeesService>(CoffeesService);
